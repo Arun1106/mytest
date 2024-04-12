6afe39bf0154d4b35e3575a01a8eea1b5ac03830
Arun1106 <52492876+Arun1106@users.noreply.github.com>
2024-04-12 11:59:24 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.45.0"
+    }
+  }
+}
+
+provider "aws" {
+
+}
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main-arun"
+  }
+}
